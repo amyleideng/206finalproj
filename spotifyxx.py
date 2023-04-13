e927a637d913b6d8e5d03aa59707181f49cacc73
Elissa Li <elissali@umich.edu>
2023-04-12 23:50:42 -0400

    set up

--- spotifyxx.py
+++ spotifyxx.py
@@ -8,3 +8,17 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 
 #get username from terminal
+username = sys.argv[1]
+
+#User ID = magicdawn89?si=f6577549ae7846b9
+
+#erase cache and prompt for user permission
+
+try:
+    token = util.prompt_for_user_token(username)
+except:
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username)
+
+#create out spotifyObject
+spotifyObject = spotipy.Spotify(auth=token)
