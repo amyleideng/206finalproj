f464c8fde802e7a00d4426b232d0385ef52f4dc5
Elissa Li <elissali@umich.edu>
2023-04-17 15:27:41 -0400

    creating database pt 1

--- brewery_api.py
+++ brewery_api.py
@@ -1,8 +1,10 @@
 import requests
 import json
+import sqlite3
+from sqlite3 import error
 
 def get_api(url):
-    params = {"limit": 100, "fields": "id,name,brewery_type.address_1,address_2,address_3,city,state_province,postal_code,country,longitude,latitude,phone,website_url,state,street", "random":True}
+    params = {"per_page": 100, "page": 1, "limit": 100, "fields": "id,name,brewery_type.address_1,address_2,address_3,city,state_province,postal_code,country,longitude,latitude,phone,website_url,state,street", "random":True}
    
     response = requests.get(url, params=params)
     if response.status_code != 200:
@@ -19,6 +21,21 @@ def get_api(url):
     except:
         print("Error parsing JSON response")
         return None
+
+def store_database():
+    #connect to the database
+    conn = sqlite3.connect('brewery_database.db')
+
+    #get a cursos from the database connection
+    cur = conn.cursor()
+
+    #select data from the User table
+    cur.execute('CREATE TABLE Breweries (Brewery, State, Type) VALUES (?, ?)')
+    for row in cur:
+        print(row)
+
+    #close the cursor
+    cur.close()
 
 def main():
     url = "https://api.openbrewerydb.org/v1/breweries"
