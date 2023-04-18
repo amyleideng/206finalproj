3e967c5acb01be4722830ef89267fc09b19c68b0
ALD <ald@AMY-LEI-DENG.local>
2023-04-18 01:51:28 -0400

    part2

--- art_api.py
+++ art_api.py
@@ -14,22 +14,32 @@ def get_api(url):
 def store_database(db, url):
     conn = sqlite3.connect(db)
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS artworks (id INT AUTO_INCREMENT PRIMARY KEY, title TEXT, origin TEXT, dept TEXT, styles TEXT, subjects TEXT)")
-    
-    artworks = get_api(url)
-    for art_dict in artworks:
-        # id = art_dict["id"]
-        title = art_dict["title"]
-        origin = art_dict["place_of_origin"]
-        dept = art_dict["department_title"]
-        styles = art_dict["style_titles"]
-        subjects = art_dict["subject_titles"]
+    cur.execute("CREATE TABLE IF NOT EXISTS artworks (id INTEGER PRIMARY KEY, id_num TEXT, title TEXT, origin TEXT, dept TEXT, styles TEXT, subjects TEXT)")
 
-        sql = "INSERT INTO artworks (title, origin, dept, styles, subjects) VALUES (?, ?, ?, ?, ?)"
-        vals = (title, origin, dept, styles, subjects)
-        cur.execute(sql, vals)
-    
-    conn.commit()
+    rows = 0
+    while rows < 25: 
+        artworks = get_api(url)
+        for art_dict in artworks:
+            cur.execute("SELECT * FROM artworks WHERE id=?", (art_dict["id"],))
+            existing_artwork = cur.fetchone()
+            if existing_artwork:
+                continue
+
+            id_num = art_dict["id"]
+            title = art_dict["title"]
+            place_of_origin = art_dict["place_of_origin"]
+            department_title = art_dict["department_title"]
+            style_titles = ", ".join(art_dict["style_titles"])
+            subject_titles = ", ".join(art_dict["subject_titles"])
+
+            sql = "INSERT INTO artworks (id_num, title, origin, dept, styles, subjects) VALUES (?, ?, ?, ?, ?, ?)"
+            vals = (id_num, title, place_of_origin, department_title, style_titles, subject_titles)
+            cur.execute(sql, vals)
+
+            conn.commit()
+            rows += 1
+            if rows >= 25:
+                break
     conn.close()
 
 
