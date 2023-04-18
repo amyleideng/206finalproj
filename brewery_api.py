a60c764fe8d487071f5bdcb80cd97969257c9f44
Elissa Li <elissali@umich.edu>
2023-04-18 16:19:37 -0400

    database setup update

--- brewery_api.py
+++ brewery_api.py
@@ -1,10 +1,11 @@
+import unittest
 import requests
 import json
+import os
 import sqlite3
-from sqlite3 import error
 
 def get_api(url):
-    params = {"per_page": 100, "page": 1, "limit": 100, "fields": "id,name,brewery_type.address_1,address_2,address_3,city,state_province,postal_code,country,longitude,latitude,phone,website_url,state,street", "random":True}
+    params = {"per_page": 100, "page": 1, "limit": 100, "fields": "id,name,brewery_type,address_1,address_2,address_3,city,state_province,postal_code,country,longitude,latitude,phone,website_url,state,street", "random":True}
    
     response = requests.get(url, params=params)
     if response.status_code != 200:
@@ -13,33 +14,49 @@ def get_api(url):
 
     try:
         brewing_data = response.json()
-        num_breweries = len(brewing_data)
-        print(f"Number of breweries: {num_breweries}")
-        for brewery in brewing_data:
-            print(brewery["name"], brewery["city"], brewery["state"], brewery["website_url"])
         return brewing_data
     except:
         print("Error parsing JSON response")
         return None
 
-def store_database():
-    #connect to the database
-    conn = sqlite3.connect('brewery_database.db')
-
-    #get a cursos from the database connection
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
+    return cur, conn
 
-    #select data from the User table
-    cur.execute('CREATE TABLE Breweries (Brewery, State, Type) VALUES (?, ?)')
-    for row in cur:
-        print(row)
+def createTable(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS breweries (id INTEGER PRIMARY KEY, id_num TEXT, name TEXT, type TEXT, city TEXT, postal_code TEXT, country TEXT, state TEXT)")
+    conn.commit()
 
-    #close the cursor
-    cur.close()
+def addBreweries(url, cur, conn):
+    rows = 0
+    while rows < 25:
+        breweries = get_api(url)
+        for brewery in breweries:
+            id_num = brewery["id"]
+            name = brewery["name"]
+            type = brewery["brewery_type"]
+            city = brewery["city"]
+            postal_code = brewery["postal_code"]
+            country = brewery["country"]
+            state = brewery["state"]
+            cur.execute(
+                """INSERT OR IGNORE INTO breweries (id, name, brewery_type, 
+                city, postal_code, country, state)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                """,
+                (id_num, name, type, city, postal_code, country, state)
+            )
+        conn.commit()
+            
 
 def main():
     url = "https://api.openbrewerydb.org/v1/breweries"
     get_api(url)
+    cur, conn = setUpDatabase("brewery_database.db")
+    createTable(cur, conn)
+    addBreweries(url, cur, conn)
 
 if __name__ == "__main__":
     main()
