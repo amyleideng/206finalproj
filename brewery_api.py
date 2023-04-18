84a952635977de5cea0c6a844cf20d56154e9f3b
Elissa Li <elissali@umich.edu>
2023-04-18 17:15:40 -0400

    finished database??

--- brewery_api.py
+++ brewery_api.py
@@ -5,7 +5,7 @@ import os
 import sqlite3
 
 def get_api(url):
-    params = {"per_page": 100, "page": 1, "limit": 100, "fields": "id,name,brewery_type,address_1,address_2,address_3,city,state_province,postal_code,country,longitude,latitude,phone,website_url,state,street", "random":True}
+    params = {"per_page": 100, "page": 1, "limit": 100, "fields": "id,name,brewery_type,city,postal_code,country,state", "random":True}
    
     response = requests.get(url, params=params)
     if response.status_code != 200:
@@ -26,37 +26,38 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def createTable(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS breweries (id INTEGER PRIMARY KEY, id_num TEXT, name TEXT, type TEXT, city TEXT, postal_code TEXT, country TEXT, state TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS breweries (id TEXT PRIMARY KEY, name TEXT, brewery_type TEXT, city TEXT, postal_code TEXT, country TEXT, state TEXT)")
     conn.commit()
 
-def addBreweries(url, cur, conn):
+def addBreweries(breweries, cur, conn):
     rows = 0
-    while rows < 25:
-        breweries = get_api(url)
-        for brewery in breweries:
-            id_num = brewery["id"]
-            name = brewery["name"]
-            type = brewery["brewery_type"]
-            city = brewery["city"]
-            postal_code = brewery["postal_code"]
-            country = brewery["country"]
-            state = brewery["state"]
-            cur.execute(
-                """INSERT OR IGNORE INTO breweries (id, name, brewery_type, 
-                city, postal_code, country, state)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-                """,
-                (id_num, name, type, city, postal_code, country, state)
+    for brewery in breweries:
+        if rows >= 25:
+            break
+        id_num = brewery["id"]
+        name = brewery["name"]
+        brewery_type = brewery["brewery_type"]
+        city = brewery["city"]
+        postal_code = brewery["postal_code"]
+        country = brewery["country"]
+        state = brewery["state"]
+        cur.execute(
+            """INSERT OR IGNORE INTO breweries (id, name, brewery_type, 
+            city, postal_code, country, state)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """,
+            (id_num, name, brewery_type, city, postal_code, country, state)
             )
-        conn.commit()
+        rows += 1
+    conn.commit()
             
 
 def main():
     url = "https://api.openbrewerydb.org/v1/breweries"
-    get_api(url)
+    breweries = get_api(url)
     cur, conn = setUpDatabase("brewery_database.db")
     createTable(cur, conn)
-    addBreweries(url, cur, conn)
+    addBreweries(breweries, cur, conn)
 
 if __name__ == "__main__":
     main()
