b985124eecd0c5150236be35d874574ec7102f53
ALD <ald@AMY-LEI-DENG.local>
2023-04-20 01:05:20 -0400

    part 2 again

--- art_api.py
+++ art_api.py
@@ -4,7 +4,7 @@ import sqlite3
 import os
 
 def get_api(url):
-    params = {"limit": 25, "fields": "id,title,place_of_origin,department_title", "random":True}
+    params = {"limit": 100, "fields": "id,title,place_of_origin,department_title", "random":True}
 
     response = requests.get(url, params=params)
     artworks_data = response.json()
@@ -23,30 +23,40 @@ def create_table(cur, conn):
                     id INTEGER PRIMARY KEY, 
                     title TEXT UNIQUE, 
                     origin_id INTEGER, 
-                    dept TEXT, 
-                    FOREIGN KEY (origin_id) REFERENCES origin(id)
+                    dept_id INTEGER, 
+                    FOREIGN KEY (origin_id) REFERENCES origin(id),
+                    FOREIGN KEY (dept_id) REFERENCES dept(id)
                     )""")
     conn.commit()
 
 def create_origin_table(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS origin (id INTEGER PRIMARY KEY AUTOINCREMENT, origin TEXT UNIQUE)")
+    cur.execute("CREATE TABLE IF NOT EXISTS origin (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, origin TEXT UNIQUE)")
+    conn.commit()
+
+def create_dept_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS dept (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dept TEXT UNIQUE)")
     conn.commit()
 
 def add_origin(url, cur, conn):
     artworks = get_api(url)
-    # for art_dict in artworks: 
-    #     cur.execute("INSERT OR IGNORE INTO origin (origin) VALUES (?)",
-    #               (art_dict["place_of_origin"]))
-
 
     origins = set()
-    for artwork in artworks:
-        origins.add(artwork['place_of_origin'])
+    for art_dict in artworks:
+        origins.add(art_dict['place_of_origin'])
 
     for idx, origin in enumerate(origins):
         cur.execute("INSERT OR IGNORE INTO origin (id, origin) VALUES (?, ?)", (idx+1, origin))
     conn.commit()
 
+def add_dept(url, cur, conn):
+    artworks = get_api(url)
+    depts = set()
+    for art_dict in artworks:
+        depts.add(art_dict["department_title"])
+
+    for idx, dept in enumerate(depts):
+        cur.execute("INSERT OR IGNORE INTO dept (id, dept) VALUES (?, ?)", (idx+1, dept))
+    conn.commit()
 
 def add_artwork(url, cur, conn):
     rows = 0
@@ -59,7 +69,7 @@ def add_artwork(url, cur, conn):
         place_of_origin = art_dict["place_of_origin"]
         department_title = art_dict["department_title"]
         
-        sql = "INSERT OR IGNORE INTO artworks (id, title, origin_id, dept) VALUES (?, ?, (SELECT id FROM origin WHERE origin = ?), ?)"
+        sql = "INSERT OR IGNORE INTO artworks (id, title, origin_id, dept_id) VALUES (?, ?, (SELECT id FROM origin WHERE origin = ?), (SELECT id FROM dept WHERE dept = ?))"
         vals = (id, title, place_of_origin, department_title)
         cur.execute(sql, vals)
 
@@ -74,8 +84,10 @@ def main():
     cur, conn = setup_database("art_institute.db")
     create_table(cur, conn)
     create_origin_table(cur, conn)
-    
+    create_dept_table(cur, conn)
+
     add_origin(url, cur, conn)
+    add_dept(url, cur, conn)
     add_artwork(url, cur, conn)
     
 
